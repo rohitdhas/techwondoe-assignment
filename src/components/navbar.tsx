561dba17951d72d6fd96e7bd04ccfbac3df0dd9b
Rohit Dhas <rohitdhas666@gmail.com>
2022-07-14 10:13:15 +0530

    added fontawesome

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
 const Navbar = () => {
   return (
     <nav className="bg-white p-5 fixed top-0 left-0 right-0">
       <ul className="flex justify-evenly w-[60%] ml-auto">
         <li>
-          About <i className="bi bi-caret-down ml-1"></i>
+          About{" "}
+          <FontAwesomeIcon className="text-sm ml-1" icon={faChevronDown} />
         </li>
         <li>Job Search</li>
         <li className="flex align items-center">
-          Candidates <i className="bi bi-caret-down ml-1"></i>
+          Candidates{" "}
+          <FontAwesomeIcon className="text-sm ml-1" icon={faChevronDown} />
         </li>
         <li className="flex align items-center">
-          Employers <i className="bi bi-caret-down ml-1"></i>
+          Employers{" "}
+          <FontAwesomeIcon className="text-sm ml-1" icon={faChevronDown} />
         </li>
         <li>Latest News</li>
         <li>Contact</li>
