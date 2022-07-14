1edc92401ead171c9df495f32e209d4131d58954
Rohit Dhas <rohitdhas666@gmail.com>
2022-07-14 14:29:52 +0530

    team section done

---
+++ src/components/team.tsx
@@ -0,0 +1,39 @@
+import Line from "../assets/line2.svg";
+
+export default function Team() {
+  const imgURL =
+    "https://images.ctfassets.net/8eswx0dgamo3/1su5M0pZpEczPj5T83VdFQ/9f53aa6d7e01151786c0a3d0aee7437f/dylan-gillis-533818-unsplash.png";
+
+  return (
+    <div className="h-[70vh] bg-[#FAE1D5] bg-opacity-[0.4] flex justify-center align items-center relative">
+      <img src={imgURL} alt="team" className="h-[300px] mr-[5%]" />
+      <img
+        src={Line}
+        alt="line"
+        className="absolute left-0 top-[-5rem] h-[95%]"
+      />
+      <div>
+        <p className="font-bold text-[#E6692E]">Our Team</p>
+        <h2 className="text-3xl font-bold my-4">A team you can trust</h2>
+        <p className="mb-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim <br />
+          vulputate aliquam arcu .Lorem ipsum dolor sit amet, consectetur <br />
+          adipiscing elit. Enim vulputate aliquam arcu .Lorem ipsum dolor sit{" "}
+          <br />
+          amet, consectetur adipiscing elit. Enim vulputate aliquam arcu .Lorem{" "}
+          <br />
+          ipsum dolor sit amet, consectetur adipiscing elit. Enim vulputate{" "}
+          <br />
+          aliquam arcu .Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+          <br />
+          Enim vulputate aliquam arcu .Lorem ipsum dolor sit amet, consectetur{" "}
+          <br />
+          adipiscing elit. Enim vulputate aliquam arcu.
+        </p>
+        <button className="px-4 py-2 bg-[#E6692E] hover:bg-orange-600 text-white rounded-sm">
+          Learn more
+        </button>
+      </div>
+    </div>
+  );
+}
