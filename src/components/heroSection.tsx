1f7ba6d16363bb10d57685753290c35f68ee094d
Rohit Dhas <rohitdhas666@gmail.com>
2022-07-15 13:18:57 +0530

    lg design done 🚀

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -5,9 +5,13 @@ export default function HeroSection() {
     "https://images.ctfassets.net/8eswx0dgamo3/1dvHDlPNVKqPNOL3LYfkOW/a727adcd3a707e7a8a2bfbdfc9dcd591/1Z2niiBPg5A.png";
 
   return (
-    <div className="mt-[4.8rem] h-[90vh] relative">
-      <img src={imgURL} alt="hero" className="h-full absolute -z-10" />
-      <img src={Line} alt="line" className="absolute right-0 h-[100%] -z-10" />
+    <div className="mt-[4.8rem] h-[60vh] md:h-[90vh] relative">
+      <img
+        src={imgURL}
+        alt="hero"
+        className="h-full md:h-full absolute -z-10"
+      />
+      <img src={Line} alt="line" className="absolute right-0 -z-10 h-[100%]" />
     </div>
   );
 }
