ec903affe46df744d7dd6f6ae94bae8c7d6160b7
Rohit Dhas <rohitdhas666@gmail.com>
2022-07-16 13:41:38 +0530

    ts fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Navbar from "./components/navbar";
-import HeroSection from "./components/heroSection";
-import WhyChooseUs from "./components/whyChooseUs";
-import Team from "./components/team";
-import News from "./components/news";
-import BottomSection from "./components/bottomSection";
-import GetInTouch from "./components/getInTouch";
-import Footer from "./components/footer";
+import Navbar from './components/navbar';
+import HeroSection from './components/heroSection';
+import WhyChooseUs from './components/whyChooseUs';
+import Team from './components/team';
+import News from './components/news';
+import BottomSection from './components/bottomSection';
+import GetInTouch from './components/getInTouch';
+import Footer from './components/footer';
 
 function App() {
   return (
--- src/utils/contentful.js
+++ src/utils/contentful.js
@@ -1,9 +1,10 @@
-import { createClient } from "contentful";
+/* eslint-disable no-undef */
+import { createClient } from 'contentful';
 
 export function useClient() {
   const client = createClient({
     space: process.env.REACT_APP_SPACE_ID,
-    accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
   });
 
   return { client };
